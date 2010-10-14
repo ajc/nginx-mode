99d5eeae470fc404a3e11663792a070e133f00c3
Andrew J Cosgriff <andrew@cosgriff.name>
2010-10-14 20:14:43 +1100

    mention where you can find it

--- nginx-mode.el
+++ nginx-mode.el
@@ -2,6 +2,8 @@
 
 ;; Copyright 2010 Andrew J Cosgriff <andrew@cosgriff.name>
 
+;; available from http://github.com/ajc/nginx-mode
+
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation; either version 2, or (at your option)
